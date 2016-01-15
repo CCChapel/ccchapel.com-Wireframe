f21bc0e21d0d5bd8fa6e998c7b199edb85bf2b01
Eric Rovtar <eric.rovtar@me.com>
2016-01-14 19:52:21 -0500

    Mobile Campus Select Functionality

---
+++ scripts/scripts.js
@@ -0,0 +1,25 @@
+//Mobile Campus Menu
+var campusSelect = ".campus-select__list";
+
+function toggleMobileCampusList() {
+    $(campusSelect).slideToggle(250);
+}
+//
+//function showCampusList() {
+//    $(campusSelect).show();
+//}
+//
+//function hideCampusList() {
+//    $(campusSelect).hide();
+//}
+
+//Mobile Menu
+var menuClass = ".banner__menu";
+
+function showMobileMenu() {
+    $(menuClass).show();
+}
+
+function hideMobileMenu() {
+    $(menuClass).hide();
+}
